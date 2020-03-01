f87538491cf77dc7372698d2a12f05547ae43021
Hans Erik Heggem <hans.erik.heggem@gmail.com>
2020-03-01 19:47:24 +0100

    wip

--- HelmState/Tools/GitTools.py
+++ HelmState/Tools/GitTools.py
@@ -3,8 +3,8 @@ import os
 from HelmState.Tools import StateHandler
 
 
-def GetRepo(stateFolder: str, resourceGroup: str, initializeIfNotExists = False):
-    stateFile = StateHandler.GetStateFilePath(stateFolder)
+def GetRepo(repoFolder: str, initializeIfNotExists=False):
+    stateFile = StateHandler.GetStateFilePath(repoFolder)
     repoFolder = os.path.dirname(stateFile)
     if not os.path.isdir(os.path.join(repoFolder, '.git')):
         if initializeIfNotExists:
@@ -13,7 +13,6 @@ def GetRepo(stateFolder: str, resourceGroup: str, initializeIfNotExists = False)
             raise Exception(f'State repoFolder {repoFolder} does not exist!')
     else:
         repo = git.Repo(repoFolder)
-    repo.index.checkout(f'rg/{resourceGroup}', force=True)
 
     return repo
 
@@ -23,17 +22,17 @@ def GetCommitCount(repo: git.Repo):
     return int(count)
 
 
-def CommitState(state: dict, stateFolder: str, resourceGroup: str, message: str):
-    StateHandler.DumpState(state, stateFolder)
-    repo = GetRepo(stateFolder, resourceGroup, initializeIfNotExists=True)
-    stateFile = StateHandler.GetStateFilePath(stateFolder)
+def CommitState(state: dict, repoFolder: str, message: str):
+    StateHandler.DumpState(state, repoFolder)
+    repo = GetRepo(repoFolder, initializeIfNotExists=True)
+    stateFile = StateHandler.GetStateFilePath(repoFolder)
 
     repo.index.add([stateFile])
     repo.index.commit(message)
 
 
-def RevertState(stateFolder: str, resourceGroup: str, numberOfCommits = 1):
-    repo = GetRepo(stateFolder, resourceGroup)
+def RevertState(repoFolder: str, resourceGroup: str, numberOfCommits=1):
+    repo = GetRepo(repoFolder)
     commitCount = GetCommitCount(repo)
     if commitCount <= numberOfCommits:
         raise Exception(f'Number of commits to revert ({numberOfCommits}) '
@@ -42,15 +41,17 @@ def RevertState(stateFolder: str, resourceGroup: str, numberOfCommits = 1):
     repo.head.reset(f'HEAD~{numberOfCommits}', index=True, working_tree=True)
 
 
-def CommitFiles(repo: git.Repo, branch: str, message: str, pushToOrigin: bool = False):
-    current = repo.create_head(branch)
-    current.checkout()
+def CommitFiles(repo: git.Repo, branch: str, message: str, pushToOrigin: bool = True):
     master = repo.heads.master
-    repo.git.pull('origin', master)
+    if pushToOrigin:
+        repo.git.pull('origin', master)
+    current = repo.git.checkout('master', b=branch)
 
-    if repo.index.diff(None) or repo.untracked_files:
-        # Changes to add and commit
-        repo.git.add(A=True)
-        repo.git.commit(m=message)
-        if pushToOrigin:
-            repo.git.push('--set-upstream', 'origin', current)
+    try:
+        if repo.index.diff(None) or repo.untracked_files:
+            repo.git.add(A=True)
+            repo.git.commit(m=message)
+            if pushToOrigin:
+                repo.git.push('--set-upstream', 'origin', current)
+    finally:
+        repo.git.checkout('master')
