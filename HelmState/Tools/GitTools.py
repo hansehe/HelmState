3f31f86d0d6a553ae078bbce29d88f19944166fa
Hans Erik Heggem <hanserik@neate.no>
2022-10-07 11:35:54 +0200

    bug fix

--- HelmState/Tools/GitTools.py
+++ HelmState/Tools/GitTools.py
@@ -121,6 +121,7 @@ def PullBranch(repo: git.Repo, branch: str,
             repo.index.reset(head=True)
             if checkoutBranchFromOrigin:
                 repo.git.checkout(remoteBranch, b=branch)
+                repo.git.push('--set-upstream', remote, branch)
             else:
                 numCommitsAhead, numCommitsBehind = GetBranchState(repo, branch)
                 if numCommitsBehind > 0:
